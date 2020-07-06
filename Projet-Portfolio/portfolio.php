4197629db337282968e6faedd6e4aa04622d6618
S-Thibault <sylvain.thibault@infamail.com>
2020-07-06 16:09:13 +0200

    Add files via upload

---
+++ Projet-Portfolio/portfolio.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+<title>Portfolio</title>
+<meta charset="utf-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <link rel="stylesheet" href="./style.css">
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+ <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+<link href="https://fonts.googleapis.com/css2?family=Bree+Serif&display=swap" rel="stylesheet">
+
+</head>
+<body>
+
+<nav class="navbar navbar-expand-md navbar-dark ml-auto">
+ <a class="navbar-brand" href="#">Portfolio</a>
+ <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+   <span class="navbar-toggler-icon"></span>
+ </button>
+ <div class="collapse navbar-collapse" id="collapsibleNavbar">
+   <ul class="navbar-nav ml-auto">
+     <li class="nav-item">
+       <a class="nav-link" href="#block1">à propos</a>
+     </li>
+     <li class="nav-item">
+       <a class="nav-link" href="#block2">projets</a>
+     </li>
+     <li class="nav-item">
+       <a class="nav-link" href="#">articles</a>
+     </li>
+     <li class="nav-item">
+       <a class="nav-link" href="#">contact</a>
+     </li>
+   </ul>
+ </div>
+</nav>
+
+<article id="block1">
+<h1> Thibault Sylvain </h1>
+<h2> Developpeur web et web mobile </h2>
+<p>texte disant ce que je fais flemme de rediger maintenant je verrais plus tard </p>
+</article>
+
+<article id="block2">
+<nav class="navbar navbar-expand-sm navbar-dark">
+  <h1>
+    mes projets
+  </h1>
+</nav>
+
+<div class="row">
+  <div class="column">
+    <img src="img/maquette.png" alt="Snow" style="">
+  </div>
+  <div class="column">
+    <img src="img/filesexplorer.png" alt="Forest" style="">
+  </div>
+  <div class="column">
+    <img src="img/bomberman.png" alt="Mountains" style="">
+  </div>
+</div>
+
+</article>
+
+
+</body>
+</html>
