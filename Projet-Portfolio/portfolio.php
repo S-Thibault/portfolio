2f1bc4907fec7e28c91166010dcb0c64aa898808
S-Thibault <sylvain.thibault@infamail.com>
2020-07-15 16:38:13 +0200

    Update portfolio.php

--- Projet-Portfolio/portfolio.php
+++ Projet-Portfolio/portfolio.php
@@ -1,6 +1,8 @@
 <?php
 include "header.php";
+include "formulaire.php"
 ?>
+
 
 <body>
 
@@ -90,6 +92,20 @@ include "header.php";
 
   <footer class="block7">
     <footer id="social">
+
+      <form id="contact" method="post" action="traitement_formulaire.php">
+	<fieldset><legend>Vos coordonnées</legend>
+		<p><label for="nom">Nom :</label><input type="text" id="nom" name="nom" /></p>
+		<p><label for="email">Email :</label><input type="text" id="email" name="email" /></p>
+	</fieldset>
+
+	<fieldset><legend>Votre message </legend>
+		<p><label for="objet">Objet :</label><input type="text" id="objet" name="objet" /></p>
+		<p><label for="message">Message :</label><textarea id="message" name="message" cols="30" rows="8"></textarea></p>
+	</fieldset>
+
+	<div style="text-align:center;"><input type="submit" name="envoi" value="Envoyer le formulaire !" /></div>
+</form>
         <div class="container">
           <ul style="list-style-type: none" class="row d-flex justify-content-center">
             <div class="row pt-4">
