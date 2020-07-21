ac49b1f703197250daf4d4d109150c72dcc35465
S-Thibault <sylvain.thibault@infamail.com>
2020-07-21 10:40:31 +0200

    Update header.php

--- Projet-Portfolio/header.php
+++ Projet-Portfolio/header.php
@@ -11,4 +11,5 @@
  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 <link href="https://fonts.googleapis.com/css2?family=Bree+Serif&display=swap" rel="stylesheet">
 <link href="https://uvc.one/upload/iblock/a4f/a4fa195b41beafcf172e2843a0bef744.css" rel="stylesheet">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr">
 </head>
